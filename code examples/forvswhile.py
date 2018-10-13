57b1fe09a7a5040f55e2b8e3f88f6f24460f3091
Unknown <23707194+DenseComet@users.noreply.github.com>
2018-10-13 14:54:29 -0400

    add for/while example

---
+++ code examples/forvswhile.py
@@ -0,0 +1,16 @@
+for i in range(0,5):
+   print(i)
+#for loops execute an action for every item in a set
+
+y = ["these","are","words"]
+for i in y:
+    print(y)
+#in this example, y is a item in the list
+#there are 3 items in the list so it will be run 3 times
+
+x = 0
+while x < 10:
+    print(x)
+    x += 1
+#while loops perform an action until a certain condition has been met
+#they are less perferable to for loops
